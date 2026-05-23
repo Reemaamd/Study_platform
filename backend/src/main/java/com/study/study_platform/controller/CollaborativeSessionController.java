46bc0d0de775a05a028ffe93d70d94ba73769e9f
Rania-saadani <raniasaadanii4@gmail.com>
2026-05-23 20:58:42 +0200

    Improve collaborative session scheduling and security

--- backend/src/main/java/com/study/study_platform/controller/CollaborativeSessionController.java
+++ backend/src/main/java/com/study/study_platform/controller/CollaborativeSessionController.java
@@ -5,8 +5,11 @@ import com.study.study_platform.service.CollaborativeSessionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.http.ResponseEntity;
 import java.util.List;
+import com.study.study_platform.dto.CollaborativeSessionDTO;
+import com.study.study_platform.model.document.StudySession;
+import org.springframework.security.core.Authentication;
 
 @RestController
 @RequestMapping("/collaborative-sessions")
@@ -18,9 +21,82 @@ public class CollaborativeSessionController {
     @GetMapping("/{groupId}/common-availabilities")
     @PreAuthorize("hasRole('USER')")
     public List<CommonAvailabilityDTO> getCommonAvailabilities(
-            @PathVariable String groupId
+            @PathVariable String groupId,
+            Authentication authentication
     ) {
 
-        return service.findCommonAvailabilities(groupId);
+        return service.findCommonAvailabilities(
+                groupId,
+                authentication.getName()
+        );
     }
+    @PostMapping
+    @PreAuthorize("hasRole('USER')")
+    public StudySession createCollaborativeSession(
+            @RequestBody CollaborativeSessionDTO dto,
+            Authentication authentication
+    ) {
+
+        String username = authentication.getName();
+
+        return service.createCollaborativeSession(
+                dto,
+                username
+        );
+    }
+    @PatchMapping("/{id}/start")
+    public ResponseEntity<?> startSession(
+            @PathVariable String id,
+            Authentication authentication
+    ) {
+
+        return ResponseEntity.ok(
+                service.startSession(
+                        id,
+                        authentication.getName()
+                )
+        );
+    }
+    @PatchMapping("/{id}/complete")
+    public ResponseEntity<?> completeSession(
+            @PathVariable String id,
+            Authentication authentication
+    ) {
+
+        return ResponseEntity.ok(
+                service.completeSession(
+                        id,
+                        authentication.getName()
+                )
+        );
+    }
+    @PatchMapping("/{id}/cancel")
+    public ResponseEntity<?> cancelSession(
+            @PathVariable String id,
+            Authentication authentication
+    ) {
+
+        return ResponseEntity.ok(
+                service.cancelSession(
+                        id,
+                        authentication.getName()
+                )
+        );
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteSession(
+            @PathVariable String id,
+            Authentication authentication
+    ) {
+
+        service.deleteSession(
+                id,
+                authentication.getName()
+        );
+
+        return ResponseEntity.ok(
+                "Session deleted successfully"
+        );
+    }
+
 }
--- backend/src/main/java/com/study/study_platform/service/CollaborativeSessionService.java
+++ backend/src/main/java/com/study/study_platform/service/CollaborativeSessionService.java
@@ -8,10 +8,20 @@ import com.study.study_platform.repository.GroupRepository;
 import com.study.study_platform.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
+import com.study.study_platform.model.document.Subject;
+import com.study.study_platform.repository.SubjectRepository;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import com.study.study_platform.model.document.StudySession;
+import com.study.study_platform.repository.StudySessionRepository;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+import java.time.LocalDateTime;
+import com.study.study_platform.dto.CollaborativeSessionDTO;
+import com.study.study_platform.model.document.StudySession;
+import com.study.study_platform.model.enums.SessionStatus;
+import com.study.study_platform.model.enums.SessionType;
 
 @Service
 @RequiredArgsConstructor
@@ -19,14 +29,37 @@ public class CollaborativeSessionService {
 
     private final GroupRepository groupRepository;
     private final UserRepository userRepository;
+    private final StudySessionRepository studySessionRepository;
+    private final SubjectRepository subjectRepository;
 
     public List<CommonAvailabilityDTO> findCommonAvailabilities(
-            String groupId
-    ) {
+            String groupId,
+            String username
+    ){
+        Utilisateur currentUser =
+                userRepository.findByUsername(username)
+                        .orElseThrow(() ->
+                                new RuntimeException(
+                                        "User not found"
+                                )
+                        );
 
         Group group = groupRepository.findById(groupId)
                 .orElseThrow(() ->
-                        new RuntimeException("Group not found"));
+                        new RuntimeException(
+                                "Group not found"
+                        )
+                );
+
+        if (!group.getMemberIds().contains(
+                currentUser.getId()
+        )) {
+
+            throw new RuntimeException(
+                    "You are not member of this group"
+            );
+        }
+        updateExpiredSessions();
 
         List<Utilisateur> members = userRepository
                 .findAllById(group.getMemberIds());
@@ -93,16 +126,429 @@ public class CollaborativeSessionService {
 
             if (valid) {
 
-                result.add(
-                        new CommonAvailabilityDTO(
-                                day,
-                                commonStart.toString(),
-                                commonEnd.toString()
-                        )
-                );
+                LocalDate today = LocalDate.now();
+
+                LocalDate weekStart =
+                        today.with(java.time.DayOfWeek.MONDAY);
+
+                LocalDate weekEnd =
+                        weekStart.plusDays(6);
+
+                LocalDate slotDate =
+                        weekStart.with(
+                                TemporalAdjusters.nextOrSame(
+                                        java.time.DayOfWeek.valueOf(day)
+                                )
+                        );
+
+                // ==========================
+                // SLOT MUST BE THIS WEEK
+                // ==========================
+                if (slotDate.isAfter(weekEnd)) {
+                    continue;
+                }
+
+                LocalDateTime slotStart =
+                        slotDate.atTime(commonStart);
+
+                LocalDateTime slotEnd =
+                        slotDate.atTime(commonEnd);
+
+                // ==========================
+                // SLOT MUST NOT BE PASSED
+                // ==========================
+                if (slotStart.isBefore(LocalDateTime.now())) {
+                    continue;
+                }
+
+                boolean occupied = false;
+
+                for (Utilisateur member : members) {
+
+                    if (hasConflict(
+                            member.getId(),
+                            slotStart,
+                            slotEnd
+                    )) {
+
+                        occupied = true;
+                        break;
+                    }
+                }
+
+                if (!occupied) {
+
+                    result.add(
+                            new CommonAvailabilityDTO(
+                                    day,
+                                    commonStart.toString(),
+                                    commonEnd.toString()
+                            )
+                    );
+                }
             }
         }
 
         return result;
+    }
+    private boolean hasConflict(
+            String userId,
+            LocalDateTime start,
+            LocalDateTime end
+    ) {
+
+        List<StudySession> sessions =
+                studySessionRepository.findByUserId(userId);
+
+        return sessions.stream().anyMatch(session ->
+
+                start.isBefore(session.getEndTime())
+                        &&
+                        end.isAfter(session.getStartTime())
+        );
+    }
+    public StudySession createCollaborativeSession(
+            CollaborativeSessionDTO dto,
+            String username
+    ) {
+        updateExpiredSessions();
+
+        Utilisateur creator = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new RuntimeException("User not found"));
+
+        Group group = groupRepository.findById(dto.getGroupId())
+                .orElseThrow(() ->
+                        new RuntimeException("Group not found"));
+        Subject subject = subjectRepository
+                .findById(dto.getSubjectId())
+                .orElseThrow(() ->
+                        new RuntimeException("Subject not found"));
+
+        if (!subject.getUserId().equals(creator.getId())) {
+
+            throw new RuntimeException(
+                    "You can only use your own subjects"
+            );
+        }
+        for (String memberId : group.getMemberIds()) {
+
+            boolean exists =
+                    subjectRepository.findAll()
+                            .stream()
+                            .anyMatch(sub ->
+
+                                    sub.getUserId().equals(memberId)
+                                            &&
+                                            sub.getName().equalsIgnoreCase(
+                                                    subject.getName()
+                                            )
+                            );
+
+            if (!exists) {
+
+                Subject newSubject = new Subject();
+
+                newSubject.setName(subject.getName());
+
+                newSubject.setUserId(memberId);
+
+                subjectRepository.save(newSubject);
+            }
+        }
+        if (!dto.getStartTime().isBefore(dto.getEndTime())) {
+
+            throw new RuntimeException(
+                    "Start time must be before end time"
+            );
+        }
+
+        if (dto.getStartTime().isBefore(
+                LocalDateTime.now()
+        )) {
+
+            throw new RuntimeException(
+                    "Cannot create session in the past"
+            );
+        }
+        String sessionDay =
+                dto.getStartTime()
+                        .getDayOfWeek()
+                        .name();
+
+        boolean validSlot = false;
+
+        List<CommonAvailabilityDTO> commonSlots =
+                findCommonAvailabilities(
+                        dto.getGroupId(),
+                        username
+                );
+
+        for (CommonAvailabilityDTO slot : commonSlots) {
+
+            LocalTime slotStart =
+                    LocalTime.parse(slot.getStartTime());
+
+            LocalTime slotEnd =
+                    LocalTime.parse(slot.getEndTime());
+
+            if (
+                    slot.getDay().equals(sessionDay)
+                            &&
+                            slotStart.equals(
+                                    dto.getStartTime().toLocalTime()
+                            )
+                            &&
+                            slotEnd.equals(
+                                    dto.getEndTime().toLocalTime()
+                            )
+            ) {
+
+                validSlot = true;
+                break;
+            }
+        }
+
+        if (!validSlot) {
+
+            throw new RuntimeException(
+                    "Session must match a valid common availability"
+            );
+        }
+        // ==============================
+        // CHECK USER IS GROUP MEMBER
+        // ==============================
+        if (!group.getMemberIds().contains(creator.getId())) {
+
+            throw new RuntimeException(
+                    "You are not member of this group"
+            );
+        }
+
+        // ==============================
+        // CHECK ALL MEMBERS CONFLICTS
+        // ==============================
+        for (String memberId : group.getMemberIds()) {
+
+            boolean conflict = hasConflict(
+                    memberId,
+                    dto.getStartTime(),
+                    dto.getEndTime()
+            );
+
+            if (conflict) {
+
+                throw new RuntimeException(
+                        "One or more members already have session conflict"
+                );
+            }
+        }
+
+        // ==============================
+        // CREATE SESSION
+        // ==============================
+        StudySession session = StudySession.builder()
+                .userId(creator.getId())
+                .groupId(group.getId())
+                .subjectId(dto.getSubjectId())
+                .participantIds(new ArrayList<>(group.getMemberIds()))
+                .startTime(dto.getStartTime())
+                .endTime(dto.getEndTime())
+                .type(SessionType.COLLABORATIVE)
+                .status(SessionStatus.PLANNED)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        return studySessionRepository.save(session);
+    }
+    private void updateExpiredSessions() {
+
+        List<StudySession> sessions =
+                studySessionRepository.findAll();
+
+        LocalDateTime now = LocalDateTime.now();
+
+        for (StudySession session : sessions) {
+
+            if (
+                    session.getType() == SessionType.COLLABORATIVE
+                            &&
+                            now.isAfter(session.getEndTime())
+            ) {
+
+                if (session.getStatus() ==
+                        SessionStatus.PLANNED) {
+
+                    session.setStatus(
+                            SessionStatus.CANCELLED
+                    );
+
+                    studySessionRepository.save(session);
+                }
+
+                else if (session.getStatus() ==
+                        SessionStatus.ONGOING) {
+
+                    session.setStatus(
+                            SessionStatus.DONE
+                    );
+
+                    studySessionRepository.save(session);
+                }
+            }
+        }
+    }
+    public StudySession startSession(
+            String sessionId,
+            String username
+    ) {
+
+        StudySession session =
+                studySessionRepository.findById(sessionId)
+                        .orElseThrow(() ->
+                                new RuntimeException(
+                                        "Session not found"));
+
+        Utilisateur user =
+                userRepository.findByUsername(username)
+                        .orElseThrow(() ->
+                                new RuntimeException(
+                                        "User not found"));
+
+        if (!session.getUserId().equals(user.getId())) {
+
+            throw new RuntimeException(
+                    "Only owner can start session"
+            );
+        }
+
+        if (session.getStatus() != SessionStatus.PLANNED) {
+
+            throw new RuntimeException(
+                    "Session cannot be started"
+            );
+        }
+        if (LocalDateTime.now().isAfter(
+                session.getEndTime()
+        )) {
+
+            throw new RuntimeException(
+                    "Session already expired"
+            );
+        }
+        if (LocalDateTime.now().isBefore(
+                session.getStartTime()
+        )) {
+
+            throw new RuntimeException(
+                    "Session cannot start before start time"
+            );
+        }
+        session.setStatus(SessionStatus.ONGOING);
+
+        return studySessionRepository.save(session);
+    }
+    public StudySession completeSession(
+            String sessionId,
+            String username
+    ) {
+
+        StudySession session =
+                studySessionRepository.findById(sessionId)
+                        .orElseThrow(() ->
+                                new RuntimeException(
+                                        "Session not found"));
+
+        Utilisateur user =
+                userRepository.findByUsername(username)
+                        .orElseThrow(() ->
+                                new RuntimeException(
+                                        "User not found"));
+
+        if (!session.getUserId().equals(user.getId())) {
+
+            throw new RuntimeException(
+                    "Only owner can complete session"
+            );
+        }
+
+        if (session.getStatus() != SessionStatus.ONGOING) {
+
+            throw new RuntimeException(
+                    "Session must be ongoing"
+            );
+        }
+
+        session.setStatus(SessionStatus.DONE);
+
+        return studySessionRepository.save(session);
+    }
+    public StudySession cancelSession(
+            String sessionId,
+            String username
+    ) {
+
+        StudySession session =
+                studySessionRepository.findById(sessionId)
+                        .orElseThrow(() ->
+                                new RuntimeException(
+                                        "Session not found"));
+
+        Utilisateur user =
+                userRepository.findByUsername(username)
+                        .orElseThrow(() ->
+                                new RuntimeException(
+                                        "User not found"));
+
+        if (!session.getUserId().equals(user.getId())) {
+
+            throw new RuntimeException(
+                    "Only owner can cancel session"
+            );
+        }
+
+        if (session.getStatus() == SessionStatus.DONE) {
+
+            throw new RuntimeException(
+                    "Completed session cannot be cancelled"
+            );
+        }
+
+        session.setStatus(SessionStatus.CANCELLED);
+
+        return studySessionRepository.save(session);
+    }
+    public void deleteSession(
+            String sessionId,
+            String username
+    ) {
+
+        StudySession session =
+                studySessionRepository.findById(sessionId)
+                        .orElseThrow(() ->
+                                new RuntimeException(
+                                        "Session not found"));
+
+        Utilisateur user =
+                userRepository.findByUsername(username)
+                        .orElseThrow(() ->
+                                new RuntimeException(
+                                        "User not found"));
+
+        if (!session.getUserId().equals(user.getId())) {
+
+            throw new RuntimeException(
+                    "Only owner can delete session"
+            );
+        }
+        if (session.getStatus() ==
+                SessionStatus.ONGOING) {
+
+            throw new RuntimeException(
+                    "Ongoing session cannot be deleted"
+            );
+        }
+
+        studySessionRepository.delete(session);
     }
 }
